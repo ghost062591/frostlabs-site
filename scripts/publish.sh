b603c77e9623c84aa05aeb0293be90f8f6574039
John <john.allisonwin@outlook.com>
2025-11-08 12:50:22 +0000

    Documented our epic Cloudflare migration journey

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -24,7 +24,7 @@ NC='\033[0m'
 
 # Configuration
 PROJECT_NAME="frostlabs-site"
-BRANCH="master"
+BRANCH="main"
 
 # Cloudflare credentials
 export CLOUDFLARE_ACCOUNT_ID="478c5f86904a5dd27b66b051cde8d51a"
